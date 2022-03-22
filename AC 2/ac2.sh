5075680e1fa4d46601af42ed5703ce60262affa1
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-22 20:35:45 +0100

    Update ac2.sh

--- AC 2/ac2.sh
+++ AC 2/ac2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 killall -9 steam
 rm -rf ~/Steam-Metadata-Editor/
-wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20II/modifications.json"
+wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%202/modifications.json"
 git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
 mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
 cd ~/Steam-Metadata-Editor/src
