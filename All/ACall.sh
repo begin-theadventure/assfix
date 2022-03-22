eebfb459435061234909b42cd47b71ecb342a476
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-22 20:37:55 +0100

    One script for all ACs.

---
+++ All/ACall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+killall -9 steam
+rm -rf ~/Steam-Metadata-Editor/
+wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/All/modifications.json"
+git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+cd ~/Steam-Metadata-Editor/src
+steammetadataeditor -s
