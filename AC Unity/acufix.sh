227ac1dcc1f44bbab7f073ada82839e772a2ea4b
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-22 20:17:28 +0100

    link

--- AC Unity/acufix.sh
+++ AC Unity/acufix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 killall -9 steam
 rm -rf ~/Steam-Metadata-Editor/
-wget ""
+wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Unity/modifications.json"
 git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
 mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
 cd ~/Steam-Metadata-Editor/src
