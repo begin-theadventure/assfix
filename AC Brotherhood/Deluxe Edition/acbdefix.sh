cd75907b3988b38b9a75681b4a46dd3fd49bbdc4
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-22 20:30:09 +0100

    Update acbdefix.sh

--- AC Brotherhood/Deluxe Edition/acbdefix.sh
+++ AC Brotherhood/Deluxe Edition/acbdefix.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 killall -9 steam
-wget "https://gist.github.com/pdfrod/7fd3c612ddd1be86b688809ffd59acbd/raw/4a12027f13a157879d59bc598eeb4d0ced2bfafa/appinfo.vdf"
-mv appinfo.vdf ~/.local/share/Steam/appcache
+rm -rf ~/Steam-Metadata-Editor/
+wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Brotherhood/Deluxe%20Edition/modifications.json"
+git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+cd ~/Steam-Metadata-Editor/src
+steammetadataeditor -s
