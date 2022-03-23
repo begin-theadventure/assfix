87748ba6b9c73f5abf377e5517d8470821682e99
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-23 10:17:10 +0100

    Remove Steam-Metadata-Editor at the end.

--- AC Brotherhood/Standard Edition/acbsefix.sh
+++ AC Brotherhood/Standard Edition/acbsefix.sh
@@ -6,3 +6,5 @@ git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Ed
 mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
 cd ~/Steam-Metadata-Editor/src
 steammetadataeditor -s
+
+rm -rf ~/Steam-Metadata-Editor/
