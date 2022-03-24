808d009a4c2479002b8ac82452bb2b86dceb00af
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-24 14:11:13 +0100

    Bunch of improvements.
    Now: it doesn't clone the entire repository, instead it just downloads the steammetadataeditor, describes what it does/did and few other command changes.

--- AC All/acallfix.sh
+++ AC All/acallfix.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
+# Start
+echo "# Assassin's Creed 2, Brotherhood and Unity fixes"
+
+echo "# Kill all Steam procceses"
 killall -9 steam
-rm -rf ~/Steam-Metadata-Editor/
+
+echo "# Remove modifications.json if already exists"
+rm modifications.json
+
+echo "# Download modifications.json for Unity"
 wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Unity/modifications.json"
-git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
-cd ~/Steam-Metadata-Editor/src
-steammetadataeditor -s
 
-cd ~/
+echo "# Download steammetadataeditor"
+wget "https://raw.githubusercontent.com/tralph3/Steam-Metadata-Editor/master/src/steammetadataeditor"
+
+echo "# Make a new directory or skip if already exists"
+mkdir -p ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Remove modifications.json if already exists"
+rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Move modifications.json to the config folder"	
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Make steammetadataeditor executable"
+chmod +x steammetadataeditor
+
+echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
+./steammetadataeditor -s
+#
+# Next
+echo "# Download modifications.json for Brotherhood Deluxe Edition"
 wget -c "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Brotherhood/Deluxe%20Edition/modifications.json"
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
-steammetadataeditor -s
 
-cd ~/
+echo "# Remove modifications.json if already exists"
+rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Move modifications.json to the config folder"	
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
+./steammetadataeditor -s
+#
+# Next
+echo "# Download modifications.json for Brotherhood Standard Edition"
 wget -c "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Brotherhood/Standard%20Edition/modifications.json"
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
-steammetadataeditor -s
 
-cd ~/
+echo "# Remove modifications.json if already exists"
+rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Move modifications.json to the config folder"	
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
+./steammetadataeditor -s
+#
+# Next
+echo "# Download modifications.json for 2"
 wget -c "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%202/modifications.json"
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
-steammetadataeditor -s
 
-rm -rf ~/Steam-Metadata-Editor/
+echo "# Remove modifications.json if already exists"
+rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Move modifications.json to the config folder"	
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
+./steammetadataeditor -s
+echo "# End"
