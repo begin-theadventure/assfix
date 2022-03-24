60a8760c23695c9293f8acb112a3811e38e309e0
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-24 14:20:31 +0100

    Bunch of improvements.
    Now: it doesn't clone the entire repository, instead it just downloads the steammetadataeditor, describes what it does/did and few other command changes.

--- AC Brotherhood/Standard Edition/acbsefix.sh
+++ AC Brotherhood/Standard Edition/acbsefix.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-killall -9 steam
-rm -rf ~/Steam-Metadata-Editor/
-wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Brotherhood/Standard%20Edition/modifications.json"
-git clone "https://github.com/tralph3/Steam-Metadata-Editor" ~/Steam-Metadata-Editor/
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
-cd ~/Steam-Metadata-Editor/src
-steammetadataeditor -s
+# Start
+echo "# Assassin's Creed Brotherhood Standard Edition fix"
 
-rm -rf ~/Steam-Metadata-Editor/
+echo "# Kill all Steam procceses"
+killall -9 steam
+
+echo "# Remove modifications.json if already exists"
+rm modifications.json
+
+echo "# Download modifications.json"
+wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%20Brotherhood/Standard%20Edition/modifications.json"
+
+echo "# Download steammetadataeditor"
+wget "https://raw.githubusercontent.com/tralph3/Steam-Metadata-Editor/master/src/steammetadataeditor"
+
+echo "# Make a new directory or skip if already exists"
+mkdir -p ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Remove modifications.json if already exists"
+rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Move modifications.json to the config folder"	
+mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+
+echo "# Make steammetadataeditor executable"
+chmod +x steammetadataeditor
+
+echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
+./steammetadataeditor -s
+
+echo "# Remove steammetadataeditor"
+rm steammetadataeditor
+echo "# End"
