7b7c9bd6351e5cd738ab94a99badfd517fabefdb
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-03-25 18:51:43 +0100

    Bunch of improvements.
    Grammar, tab, mainly visual.

--- AC 2/ac2fix.sh
+++ AC 2/ac2fix.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
+# Start
 echo "# Assassin's Creed 2 fix"
 
-echo "# Kill all Steam procceses"
-killall -9 steam
+echo "# Kill all Steam processes"
+	killall -9 steam
 
-echo "# Remove modifications.json if already exists"
-rm modifications.json
+echo "# Remove modifications.json if it already exists"
+	rm modifications.json
 
-echo "# Remove steammetadataeditor if already exists"
-rm steammetadataeditor
+echo "# Remove modifications.json from the config folder if it already exists"
+	rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+
+echo "# Remove steammetadataeditor if it already exists"
+	rm steammetadataeditor
 
 echo "# Download modifications.json"
-wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%202/modifications.json"
+	wget "https://raw.githubusercontent.com/begin-theadventure/acfix/main/AC%202/modifications.json"
 
 echo "# Download steammetadataeditor"
-wget "https://raw.githubusercontent.com/tralph3/Steam-Metadata-Editor/master/src/steammetadataeditor"
+	wget "https://raw.githubusercontent.com/tralph3/Steam-Metadata-Editor/master/src/steammetadataeditor"
 
-echo "# Make a new directory or skip if already exists"
-mkdir -p ~/.local/share/Steam-Metadata-Editor/config
-
-echo "# Remove modifications.json if already exists"
-rm ~/.local/share/Steam-Metadata-Editor/config/modifications.json
+echo "# Create a config directory or skip it if it already exists"
+	mkdir -p ~/.local/share/Steam-Metadata-Editor/config
 
 echo "# Move modifications.json to the config folder"	
-mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
+	mv modifications.json ~/.local/share/Steam-Metadata-Editor/config
 
 echo "# Make steammetadataeditor executable"
-chmod +x steammetadataeditor
+	chmod +x steammetadataeditor
 
 echo "# Execute steammetadataeditor and save modifications.json to appinfo.vdf"
-./steammetadataeditor -s
+	./steammetadataeditor -s
 
+echo "# Cleanup.."
+echo "# Remove the Steam-Metadata-Editor folder"
+	rm -dr ~/.local/share/Steam-Metadata-Editor/
 echo "# Remove steammetadataeditor"
-rm steammetadataeditor
+	rm steammetadataeditor
+
 echo "# End"
